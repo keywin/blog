00965542c067895a832fedab3128400f9a75b824
keywin <liukaiwen@lumlord.com>
2020-01-19 18:01:15 +0800

    列表页

---
+++ src/components/ListPages.vue
@@ -0,0 +1,36 @@
+<!--
+-- @Author: keywin
+-- @StartingTime: 2020-01-19 17:53:36
+-- @script: 列表页
+-->
+<template>
+  <div class="listPages">
+		<h3 class="titl">Oracle</h3>
+		<div class="miaoshu">
+			作者: keywin / 发布于: 2019-12-21 / 本文属于: 前端 / 标签: 前端
+		</div>
+		<div class="cont">
+			工作知识点记录
+		</div>
+		<div class="detail">READ</div>
+	</div>
+</template>
+<script>
+export default {
+  name: "ListPages",
+  data() {
+    return {};
+  },
+  props: [],
+  watch: {},
+  computed: {},
+  components: {},
+  created() {},
+  methods: {},
+  mounted() {},
+  updated() {},
+  destroyed() {}
+};
+</script>
+<style lang="scss" scoped>
+</style>
