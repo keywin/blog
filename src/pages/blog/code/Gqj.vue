753bb7fa79c9d85f23494d0a9850af45741c1632
keywin <18404974906@163.com>
2020-02-12 21:14:50 +0800

    优化滚动条样式,添加宫崎骏详情页

--- src/pages/blog/code/Gqj.vue
+++ src/pages/blog/code/Gqj.vue
@@ -1,8 +1,95 @@
 <template>
   <div class="detail detail_col">
 		<h3 class="titl">{{ detailData.titl }}</h3>
-		<div class="detailCont tx-c">
-			<p class="mt30">宫崎骏，1941年1月5日出生于东京都文京区 [1]  ，日本动画师、动画制作人、漫画家、动画导演、动画编剧。</p>
+		<div class="detailCont">
+			<p class="mt30 tx-c">宫崎骏，是我最喜欢的日本动画师、动画制作人、漫画家、动画导演、动画编剧。</p>
+			<p class="mt30 tx-c">日漫由岸本齐史入门, 因宫崎骏而热爱。</p>
+			<p class="mt30 tx-c">下面是看过的宫崎骏日漫, 百度整理了一些语录</p>
+			<div class='imgFoo mt30 tx-c' style="max-height: fit-content;">
+				<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_mdbe.jpg" alt="">
+			</div>
+			<p class="mt30 tx-r">生命可以随心所欲，但不能随波逐流。</p>
+			<p class="mt30 tx-r">你应该要学着做你自己，面对真实的自我，只要做到这一点，你就不会害怕任何事情了。</p>
+			<p class="mt30 tx-r">—— 《猫的报恩》</p>
+			<div style="display: flex;">
+				<div style="flex: 1;padding-right: 10px;">
+					<div class='imgFoo mt30 tx-l' style="max-height: fit-content;">
+						<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_fzg.jpg" alt="">
+					</div>
+					<p class="mt30">最远的旅行，是从自己的身体到自己的心，是从一个人的心到另一个人的心。坚强不是面对悲伤不流一滴泪，而是擦干眼泪后微笑面对以后的生活。</p>
+					<p class="mt30 tx-r">—— 《风之谷》</p>
+				</div>
+				<div style="flex: 1;padding-left: 10px;border-left: 1px solid #e9e9eb;">
+					<div class='imgFoo mt30 tx-c' style="max-height: fit-content;">
+						<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_qyqx.gif" alt="">
+					</div>
+					<p class="mt30">人生就是一列开往坟墓的列车，路途上会有很多站，很难有人可以自始至终陪着走完。当陪你的人要下车时，即使不舍也该心存感激，然后挥手道别。</p>
+					<p class="mt30">不管前方的路有多痛苦，只要走的方向正确，不管那多么崎岖不平，都比站在原地要更加接近幸福。</p>
+					<p class="mt30 tx-r">—— 《千与千寻》</p>
+				</div>
+			</div>
+			<div class='imgFoo mt30 tx-c' style="max-height: fit-content;">
+				<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_mnzjb.gif" alt="">
+			</div>
+			<p class="mt30 tx-c">成长是一笔交易，我们都是用朴素的童真与未经人事的洁白交换长大的勇气。</p>
+			<p class="mt30 tx-c">在这个世界上别太依赖任何人，因为当你在黑暗中挣扎时，连你的影子都会离开你。</p>
+			<p class="mt30 tx-r">—— 《魔女宅急便》</p>
+			<div style="display: flex;">
+				<div style="flex: 1;padding-right: 10px;">
+					<div class='imgFoo mt30 tx-c' style="max-height: fit-content;">
+						<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_lm02.jpg" alt="">
+					</div>
+					<p class="mt30">生活坏到一定程度就会好起来，因为它无法更坏。努力过后，才知道许多事情，坚持坚持，就过来了。</p>
+					<p class="mt30 tx-r">—— 《龙猫》</p>
+				</div>
+				<div style="flex: 1;padding-left: 10px;border-left: 1px solid #e9e9eb;">
+					<div class='imgFoo mt30 tx-c' style="max-height: fit-content;">
+						<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_ylgz03.jpg" alt="">
+					</div>
+					<p class="mt30 tx-l">到不了的地方都叫做远方，回不去的世界都叫做家乡，我一直向往的却是比远更远的地方。</p>
+					<p class="mt30 tx-l">我们的生命太短，来不及见证那些遥远到令人恍惚的词语，比如天长地久，海角天涯，碧落黄泉，沧海桑田。 所以，要活下去。</p>
+					<p class="mt30 tx-l">人都会死的，只不过是早晚的问题，何必太伤感呢。</p>
+					<p class="mt30 tx-c">—— 《幽灵公主》</p>
+				</div>
+			</div>
+			<div class='imgFoo mt30 tx-c' style="max-height: fit-content;">
+				<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_xysdjyj.jpg" alt="">
+			</div>
+			<p class="mt30 tx-c">我浑身伤痛，跋涉万里，疲倦地寻找，只为来与你相见。</p>
+			<p class="mt30 tx-c">我不知道有多喜欢你，但去见你一定是用跑着的。</p>
+			<p class="mt30 tx-r">—— 《悬崖上的金鱼姬》</p>
+			<div style="display: flex;">
+				<div style="flex: 1;padding-right: 10px;border-right: 1px solid #e9e9eb;">
+					<div class='imgFoo mt30 tx-c' style="max-height: fit-content;">
+						<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_jdxdxrala.gif" alt="">
+					</div>
+					<p class="mt30">欣赏夏日雨后的天空，在花园里为你摘下一朵小花。穿过小径，那里是另外一个世界，蝴蝶在空中飞舞，然而有人在那里等我吗。</p>
+					<p class="mt30">你已经是我心脏的一部分了。因为借走的是糖，还回的是心。</p>
+					<p class="mt30">许多时候，让我们放不下的，其实并不是对方，而是那些逝去的共同回忆。</p>
+					<p class="mt30 tx-r">—— 《借东西的小人阿莉埃蒂》</p>
+					<div class='imgFoo mt30 tx-c' style="max-height: fit-content;">
+						<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_qfl.gif" alt="">
+					</div>
+					<p class="mt30">已经这么久了，我为何依然不愿让你静静离去，依然对你渴求不已。</p>
+					<p class="mt30">可我要怎样做，才能带着现在这不安的情绪，重新走到故事里描绘的我们那幸福的日子里去呢？</p>
+					<p class="mt30">已经这么久了，我为何依然不愿让你静静离去，依然对你渴求不已。</p>
+					<p class="mt30 tx-r">—— 《起风了》</p>
+				</div>
+				<div style="flex: 1;padding-left: 10px;">
+					<div class='imgFoo mt30 tx-r' style="max-height: fit-content;">
+						<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_tkzc.jpg" alt="">
+					</div>
+					<p class="mt30">最远的旅行，是从自己的身体到自己的心，是从一个人的心到另一个人的心。坚强不是面对悲伤不流一滴泪，而是擦干眼泪后微笑面对以后的生活。</p>
+					<p class="mt30 tx-r">—— 《天空之城》</p>
+				</div>
+			</div>
+			<div class='imgFoo mt30 tx-c' style="max-height: fit-content;">
+				<img src="http://q4ujnj8md.bkt.clouddn.com/gqj_hedydcb.jpg" alt="">
+			</div>
+			<p class="mt30 tx-l">世界这么大，人生这么长，总有一个人，让你想要温柔对待。就是因为你不好，才要留在你身边，给你幸福。对不起，你一直等我，我却到现在才来。</p>
+			<p class="mt30 tx-l">人生中遇到的磨难很多，我们愈是成熟，也愈是胆小，反倒在绝境中才能激发出内心的潜能，从而变得更加坚强和勇敢。</p>
+			<p class="mt30 tx-l">被一个人深深地爱著将给你力量;深深地爱著一个人将给你勇气。</p>
+			<p class="mt30 tx-r">—— 《哈尔的移动城堡》</p>
 		</div>
 		<div class="detailFooter">
 			<p class="date">
